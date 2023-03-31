4eb9e499f0e56a7d1110a1552957a2a9e209b476
swaroop chandra <swaroop.chandra@taskmo.com>
2023-03-31 11:11:12 +0530

    data merge

--- controller/liquide/liquideGet.js
+++ controller/liquide/liquideGet.js
@@ -4,26 +4,38 @@ const responseHandler = require("../../utils/responseHandler");
 const getTrades = (req, res, next) => {
   const queryData = req.query;
   let userData = "";
-  if (queryData?.type || queryData?.user_id) {
+  if (queryData?.type) {
+    userData = `where type='${queryData.type}' `;
+  } else if (queryData?.user_id) {
+    userData = `where  user_id=${queryData.user_id}`;
+  } else {
     userData = `where type='${queryData.type}' and user_id=${queryData.user_id}`;
   }
 
-  readPool.query(
-    `SELECT id,
-  type,
-  user_id,
-  symbol,
-  shares,
-  price,
-  timestamp from stock_trade ${userData}`,
-    (error, trades) => {
-      if (error) {
-        responseHandler.errorHandle(res, error);
-      } else {
-        responseHandler.successDataResponse(res, "trades_list", trades);
+  if (
+    queryData?.type == "buy" ||
+    queryData?.type == "sell" ||
+    queryData.user_id
+  ) {
+    readPool.query(
+      `SELECT id,
+    type,
+    user_id,
+    symbol,
+    shares,
+    price,
+    timestamp from stock_trade ${userData}`,
+      (error, trades) => {
+        if (error) {
+          responseHandler.errorHandle(res, error);
+        } else {
+          responseHandler.successDataResponse(res, "trades_list", trades);
+        }
       }
-    }
-  );
+    );
+  } else {
+    responseHandler.errorHandle(res, "trades_list", trades);
+  }
 };
 
 const getSigleTrades = (req, res, next) => {
@@ -42,7 +54,11 @@ const getSigleTrades = (req, res, next) => {
       if (error) {
         responseHandler.errorHandle(res, error);
       } else {
-        responseHandler.successDataResponse(res, "trade_data", trades[0]);
+        if (trades.length > 0) {
+          responseHandler.successDataResponse(res, "trade_data", trades[0]);
+        } else {
+          responseHandler.errorHandle(res, "Invalid number");
+        }
       }
     }
   );
